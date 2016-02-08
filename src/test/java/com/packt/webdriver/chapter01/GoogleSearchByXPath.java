d52410ad028677b6d84cbdadb87c49e8f494ab10
chintan <06.chintan@gmail.com>
2016-02-08 01:18:22 +0000

    XPath

--- src/test/java/com/packt/webdriver/chapter01/GoogleSearchByXPath.java
+++ src/test/java/com/packt/webdriver/chapter01/GoogleSearchByXPath.java
@@ -1,9 +1,23 @@
 package com.packt.webdriver.chapter01;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Created by chintan on 08/02/2016.
  */
 public class GoogleSearchByXPath {
+    public static void main (String[] args)
+    {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        WebElement searchBox = driver.findElement(By.xpath("//*[@id='lst-ib']"));
+        searchBox.sendKeys("Searching the internet at midnight");
+        searchBox.submit();
 
+
+    }
 
 }
